a9cf38a46e1619e178f9f1eddb31f48028f4fa68
Katie Moore <katie.moore@bynd.com>
2018-10-10 16:32:51 +0100

    added error pages, ability to register and edit profiles

--- app/models.py
+++ app/models.py
@@ -1,15 +1,62 @@
-from app import db
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+      'User', secondary=followers,
+      primaryjoin=(followers.c.follower_id == id),
+      secondaryjoin=(followers.c.followed_id == id),
+      backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def follow(self, user):
+        if not self.is_following(user):
+          self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+          self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        # query the posts table for all posts from a user that is followed by anyone
+        followed =  Post.query.join(followers, (followers.c.followed_i == Post.user_id)
+            # from this combined temporary table filter for those posts that belong to users the current user follows
+            ).filter(followers.c.follower_id == self.id)
+
+        # get current users own posts
+        own = Post.query.filter_by(user_id=self.id)
+
+        # use union to combine users own posts and the posts of those their following, sort by date
+        return followed.union(own).order_by(Post.timestamps.desc())
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
